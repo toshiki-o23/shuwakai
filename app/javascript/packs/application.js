fbd02ded2d609111e0b1c53be65b184e58c1d723
toshiki-o23 <tsk.okmt.15@gmail.com>
2021-05-04 21:07:46 +0900

    [add] bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,8 @@
+// bootstrapパッケージを使用
+import 'bootstrap';
+// stylesheet/applicationファイルによりbootstrapに対応
+import '../stylesheets/application';
+
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
