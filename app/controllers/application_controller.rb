219b292e35665fc513e51b4cb54a8d84ee6cef6f
toshiki-o23 <tsk.okmt.15@gmail.com>
2021-05-04 21:43:51 +0900

    [add] usersテーブルにnameカラム追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::Base
   # ログインしてない場合非表示
   before_action :authenticate_user!
+
+  # ストロングパラメーターを指定
+  protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    end
 end
